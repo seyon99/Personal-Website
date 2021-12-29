e9865a4edbd3aa32da81d21f68376a50679dd1c0
seyon99 <seyonkuganesan@gmail.com>
2021-12-29 11:49:27 -0500

    edited about section

--- src/Components/About.js
+++ src/Components/About.js
@@ -27,7 +27,7 @@ class About extends Component {
 
             <p>Hello! I Study Computer Science at the University of Toronto and I am passionate about software development. 
                I have interned at <a href="https://www.ciena.com/" target="_blank">Ciena</a> and <a href="https://www.interac.ca/en/" target="_blank">Interac</a> in Software Engineering and Site Reliability Engineering roles respectively.</p>
-            <p>Apart from coding, my hobbies include listening to music and podcasts, playing basketball, and playing sandbox games such as Minecraft. Thanks for visiting my website, and please feel free to contact me.</p>
+            <p>Apart from coding, my hobbies include listening to music and podcasts, and playing sandbox games such as Minecraft. Thanks for visiting my website, and please feel free to contact me!</p>
             {/*
                <div className="columns download">
                   <p>
