48a69ef0950bdfd1cd97c37c13548d1a16f4eb79
seyon99 <seyonkuganesan@gmail.com>
2021-12-28 23:16:05 -0500

    added favicon and removed various files

--- src/Components/About.js
+++ src/Components/About.js
@@ -20,13 +20,14 @@ class About extends Component {
       <section id="about">
       <div className="row">
          <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Tim Baker Profile Pic" />
+            <img className="profile-pic"  src={profilepic} alt="Seyon Profile Pic" />
          </div>
          <div className="nine columns main-col">
             <h2>About Me</h2>
 
-            <p>{bio}</p>
-            <div className="row">
+            <p>Hello! I Study Computer Science at the University of Toronto and I am passionate about software development. 
+               I have interned at Ciena and Interac in Software Engineering and Site Reliability Engineering roles respectively.</p>
+            {/* <div className="row">
                <div className="columns contact-details">
                   <h2>Contact Details</h2>
                   <p className="address">
@@ -43,7 +44,7 @@ class About extends Component {
                      <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
                   </p>
                </div>
-            </div>
+            </div> */}
          </div>
       </div>
 
