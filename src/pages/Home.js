15d4bb14ca15c1a6bae816364c3530e8120a5fc0
seyon99 <seyonkuganesan@gmail.com>
2022-03-12 18:20:49 -0500

    foreground functional

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,25 @@
-import React, {useEffect, useState} from "react";
-import './Home.css'
+import React from 'react'
+import BIRDS from 'vanta/dist/vanta.birds.min'
+// Make sure window.THREE is defined, e.g. by including three.min.js in the document head using a <script> tag
 
-function Home(){
-  return(
-    <h1>Homepage placeholder</h1>
-  )
+class Home extends React.Component {
+  constructor() {
+    super()
+    this.vantaRef = React.createRef()
+  }
+  componentDidMount() {
+    this.vantaEffect = BIRDS({
+      el: this.vantaRef.current
+    })
+  }
+  componentWillUnmount() {
+    if (this.vantaEffect) this.vantaEffect.destroy()
+  }
+  render() {
+    return <div style={{height: "100vh", width: "100wh", zIndex: -1}} ref={this.vantaRef}>
+      <span style={{color: 'white'}}>Seyon Kuganesan</span>
+    </div>
+  }
 }
+
 export default Home;
