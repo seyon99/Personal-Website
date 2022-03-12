8fe974ade8c42d1997119826c265e28bf4e66a08
seyon99 <seyonkuganesan@gmail.com>
2022-03-12 17:26:06 -0500

    removed gradient

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { makeStyles, ThemeProvider, createTheme } from '@material-ui/core/styles';
+import Typed from "typed.js";
+import { useEffect, useRef } from "react";
 
 // I'm using the following colour palette:
 // https://coolors.co/palette/f72585-b5179e-7209b7-560bad-480ca8-3a0ca3-3f37c9-4361ee-4895ef-4cc9f0
@@ -20,10 +22,37 @@ const useStyles = makeStyles({
 
 function Experience() {
     const classes = useStyles();
+
+    const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["Handy", "Mandy", "Candy", "More Strings"], // Strings to display
+      // Speed settings, try diffrent values untill you get good results
+      startDelay: 300,
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 100,
+      smartBackspace: true,
+      loop: true,
+      showCursor: true,
+      cursorChar: "!"
+    });
+
+    // Destropying
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
+
     return (
         <>
             <div className={classes.root}>
                 <span className={classes.pgTitle}>Work Experience</span>
+                <br></br>
+                <span>I am typing the following:</span>
+                <span ref={el}></span>
             </div>
         </>
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,39 +1,9 @@
 import React, {useEffect, useState} from "react";
 import './Home.css'
 
-class Home extends React.Component {
-    constructor (props) {
-      super(props);
-      this.state = {
-        animationClass: 'test'
-      }
-      this.changeState = this.changeState.bind(this);
-    }
-    
-    changeState(){
-      if(this.state.animationClass === 'test'){
-        this.setState({
-          animationClass: 'test paused'
-        });
-      }else{
-        this.setState({
-          animationClass: 'test'
-        });
-      }
-    }
-    render() {
-      return(
-      <div className={this.state.animationClass}>
-        <h1 className="homeTitle">Seyon Kuganesan</h1>
-        {/* <p>Hello! I Study Computer Science at the University of Toronto and I am passionate about software development. I have interned at Ciena and Interac in Software Engineering and Site Reliability Engineering roles respectively.
-            Thanks for visiting my website, and please feel free to contact me!
-            </p> */}
-      </div>
-      );
-    }
-  }
-  
-  /*
-   * Render the above component into the div#app
-   */
+function Home(){
+  return(
+    <h1>Homepage placeholder</h1>
+  )
+}
 export default Home;
