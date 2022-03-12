6b2a670c8eb624569b2b9f9f0a346e6c0829f563
seyon99 <seyonkuganesan@gmail.com>
2022-03-12 13:57:11 -0500

    refactored react web init commit

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState, } from "react";
+
+function Home(){
+    return(
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '90vh'
+        }}>
+            <h1>Home (Temp Placeholder)</h1>
+            <h5>TODO: implement</h5>
+        </div>
+    )
+}
+
+export default Home;
