e395e31d75a54cf58cea67bb4437c0fd413e1cfd
seyon99 <seyonkuganesan@gmail.com>
2022-03-12 20:16:14 -0500

    made stuff clickable

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import BIRDS from 'vanta/dist/vanta.birds.min'
+import React from 'react';
+import BIRDS from 'vanta/dist/vanta.birds.min';
+import { InsertDriveFile, GitHub, LinkedIn, Email } from '@mui/icons-material';
 
 class Home extends React.Component {
   constructor() {
@@ -15,7 +16,8 @@ class Home extends React.Component {
     if (this.vantaEffect) this.vantaEffect.destroy()
   }
   render() {
-    return <div style={{ height: "100vh", width: "100wh", zIndex: -1 }} ref={this.vantaRef}>
+    // might need to make use of zIndex: -1 in vantaRef sx
+    return <div style={{ height: "100vh", width: "100wh" }} ref={this.vantaRef}> 
       <h1 style={{ color: 'white', fontSize: 100, display: "inline", margin: 0, position: "relative", top: "30%", paddingLeft: 50 }}>
         Hey 👋, I'm Seyon
       </h1>
@@ -30,6 +32,10 @@ class Home extends React.Component {
         Thanks for visiting my website, and feel free to reach out.
       </span>
       {/* can make links and stuff clickable/highlightable by doing some z-index shenanigans */}
+      <br></br><br></br>
+      <span style={{ fontSize: 'medium',color: 'white', display: 'inline-block', maxWidth: "50%", position: "relative", top: "35%", paddingLeft: 50 }}>
+      <GitHub /> <InsertDriveFile /> <LinkedIn /> <Email />
+      </span>
     </div>
   }
 }
